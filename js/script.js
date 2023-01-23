043708ab167c3caf552a607d92f1d3b613783fba
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2023-01-23 15:11:43 -0500

    change

--- js/script.js
+++ js/script.js
@@ -10,23 +10,37 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-01-HTML/sw.js", {
-    scope: "/ICS2O-Unit5-01-HTML/",
+  navigator.serviceWorker.register("/ICS2O-Unit6-03-HTML/sw.js", {
+    scope: "/ICS2O-Unit6-03-HTML/",
   })
 }
 
-const randomNumber = Math.floor(Math.random() * 6) + 1
+;("use strict")
 
-function updateSliderValue(valueFromSlider) {
-  document.getElementById("slider-value").innerHTML = valueFromSlider
+const weather2 = async (URLAddress) => {
+  try {
+    const request = await fetch(URLAddress)
+    const jsonData = await request.json()
+    let tempature1 = jsonData.main.temp
+    let tempature2 = 0
+    const feeling = jsonData.weather[0]
+    const image = feeling.icon
+    tempature2 = tempature1 - 273.15
 
-  if (valueFromSlider == randomNumber) {
-    document.getElementById("answer").innerHTML =
-      "the answer was, " + randomNumber + "!" + " You got it! Excellent."
-  }
+    console.log(jsonData.weather)
+    document.getElementById("theImage").innerHTML =
+      "<img src='https://openweathermap.org/img/wn/" +
+      image +
+      "@2x.png' alt='Icon' width='10%'><br><h5>"
+    ;(">")
 
-  if (valueFromSlider != randomNumber) {
-    document.getElementById("answer").innerHTML =
-      "Sorry the answer was, " + randomNumber + "!" + " please try again."
+    document.getElementById("theTemp").innerHTML =
+      "The tempurature outside is " + tempature2.toFixed(2) + " °C!"
+  } catch (err) {
+    console.log(err)
   }
 }
+
+weather2(
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+)
